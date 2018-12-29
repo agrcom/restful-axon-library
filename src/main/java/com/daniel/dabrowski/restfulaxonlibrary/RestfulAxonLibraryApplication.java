b8618edff220df8c60415a2005d8402e48aae6e7
Daniel Dabrowski <agrcom@yahoo.pl>
2018-12-29 11:15:03 +0100

    first commit

---
+++ src/main/java/com/daniel/dabrowski/restfulaxonlibrary/RestfulAxonLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.daniel.dabrowski.restfulaxonlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulAxonLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulAxonLibraryApplication.class, args);
+	}
+
+}
+
